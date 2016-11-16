dfa47d703a63f7020721fffd785a61e7e37aacb6
lukitree <lukitree@gmail.com>
2016-11-16 15:02:18 -0700

    Testing

--- testing/main.cpp
+++ testing/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <string
+
+class Test
+{
+public:
+	Test()
+	{
+		std::cout << "Test()" << std::endl;
+	}
+
+	meow(std::string msg)
+	{
+		std::cout << "Meow, " << msg << ", Meow!" << std::endl;
+	}
+}
 
 int main()
 {
 	std::cout << "Hello world!" << std::endl;
+
+	Test * test = new Test();
+	test->meow();
 
 	return 0;
 }
