2f6bfe2ee864ca5478ed72be38cc41189358fb25
lukitree <lukitree@gmail.com>
2016-11-16 15:51:08 -0700

    Update

--- testing/main.cpp
+++ testing/main.cpp
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <linux/i2c-dev.h>
 
 int main()
 {
 	char filename[20];
 	sprintf(filename, "/dev/i2c-%d", 1);
-	FILE file;
-	file = open(filename, O_RDWR);
+	int file = open(filename, O_RDWR);
 
 	return 0;
 }
