dd69023fb9a3ba14a6ee324f21929f4e53dac15b
lukitree <lukitree@gmail.com>
2016-11-16 18:21:14 -0700

    Update

---
+++ testing/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <linux/i2c-dev.h>
+
+void selectDevice(int file, int addr);
+
+int main()
+{
+	char filename[20];
+	sprintf(filename, "/dev/i2c-%d", 1);
+	int file = open(filename, O_RDWR);
+	if(file < 0)
+	{
+		printf("Unable to open I2C bus!");
+		exit(1);
+	}
+	printf("Success!\n");
+
+	selectDevice(file, ACC_ADDRESS);
+
+	return 0;
+}
+
+void selectDevice(int file, int addr)
+{
+	char device[3];
+
+	if(ioctl(file, I2C_SLAVE, addr) < 0)
+	{
+		printf("Failed to select I2C device.");
+	}
+}
